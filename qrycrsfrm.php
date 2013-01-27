e5935450aea01730f8d31374732ed61968142673
PichuChen <Pichubaby@gmail.com>
2013-01-27 16:34:47 +0800

    Add class

---
+++ qrycrsfrm.php
@@ -0,0 +1,15 @@
+<?php
+class QryCrsFrm{
+
+function getClassofDepartment($D1){
+	return require('getClassofDepartment.php');
+}
+function getDepartmentList(){
+	return require('getDepartmentList.php');
+}
+
+
+};
+
+
+?>
