2c0d8f669d7df70c10dc5203b63230d7d338836b
PichuChen <Pichubaby@gmail.com>
2013-07-08 14:46:42 +0800

    format HIS2

--- getClassofDepartment.php
+++ getClassofDepartment.php
@@ -48,10 +48,14 @@ for($i=0;$i<$itemLength;$i += 20){
 		//$ans[] = $depList->item(3 + $j)->nodeValue;
 		$tmp = array();
 		$classItem = $depList->item(3 + $j)->getElementsByTagName('td');
+		
 		foreach($classItem as $item){
 			$tmp[] = $item->nodeValue;
 		}
-		$ans[] = $tmp;
+		
+
+		
+		$ans[] = formatHIS2($tmp);
 
 	}
 	
@@ -61,5 +65,34 @@ for($i=0;$i<$itemLength;$i += 20){
 
 
 return $ans;
+
+function formatHIS2($tmp){
+
+	$tmp2 = array() ;
+	$tmp2['year'] = $tmp[0];
+	$tmp2['semester'] = $tmp[1];
+	$tmp2['department'] = $tmp[2];
+	$tmp2['coursecode'] = $tmp[3];
+	$tmp2['grade'] = $tmp[4];
+	$tmp2['class'] = $tmp[5];
+	$tmp2['subject'] = $tmp[6];
+	$tmp2['credit'] = $tmp[7];
+	$tmp2['FS'] = $tmp[8];
+	$tmp2['RS'] = $tmp[9];
+	$tmp2['CN'] = $tmp[10];
+	$tmp2['teacher'] = explode(',',$tmp[11]);
+	$tmp2['location'] = $tmp[12];
+	$tmp2['time'] = array(
+			'mon' => $tmp[13],
+			'tue' => $tmp[14],
+			'wed' => $tmp[15],
+			'thu' => $tmp[16],
+			'fri' => $tmp[17],
+			'sat' => $tmp[18],
+			'sun' => $tmp[19],
+			);
+	$tmp2['ps'] = $tmp[20];
+	return $tmp2;
+}
 
 ?>
